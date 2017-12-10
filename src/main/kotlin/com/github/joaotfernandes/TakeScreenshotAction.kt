a9d5421ac8a3f0e52116fd38c91417be0187f60e
Joao Fernandes <jtfernandes93@gmail.com>
2017-12-10 15:12:14 +0000

    Add the TakeScreenshot action to android tools group
    Add the TakeScreenshot action to android tools group that will be shown last in that list with a suggestive camera icon

--- src/main/kotlin/com/github/joaotfernandes/TakeScreenshotAction.kt
+++ src/main/kotlin/com/github/joaotfernandes/TakeScreenshotAction.kt
@@ -3,9 +3,8 @@ package com.github.joaotfernandes;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 
-class TakeScreenshotAction : AnAction() {
+class TakeScreenshotAction : AnAction(ScreenshortIcons.SCREENSHOT_ICON) {
 
     override fun actionPerformed(event: AnActionEvent) {
     }
-
 }
